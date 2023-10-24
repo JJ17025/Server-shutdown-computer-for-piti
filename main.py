77143eccce095fe981375598ea28a2c9b306f212
jj <jaknarong.spk02@gmail.com>
2023-10-24 19:17:34 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'นี้คือ main<br>ไปที่ http://127.0.0.1:5000/shutdown'
+
+
+@app.route('/shutdown')
+def shutdown():
+    os.system("shutdown /s /t 1")
+    return 'shutdown ok'
+
+
+if __name__ == '__main__':
+    app.run()
